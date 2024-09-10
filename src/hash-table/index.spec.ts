ada0ac5074c79f18160b0fb9ed49a603d614c72d
Joe Carpenito <johcarpe1@WKMZT2D96F6E.global.publicisgroupe.net>
2024-09-10 15:27:58 -0400

    flesh out the hash table

--- src/hash-table/index.spec.ts
+++ src/hash-table/index.spec.ts
@@ -18,13 +18,33 @@ describe('HashTable', () => {
     hashTable.insert('Roger Rabbit', 'framed by Judge Doom')
     hashTable.remove('Roger Rabbit')
     expect(hashTable.lookup('Roger Rabbit')).toBeUndefined()
+
+    hashTable.insert('abc', 'framed by Judge Doom')
+    hashTable.insert('acb', 'married to Roger Rabbit')
+    hashTable.remove('abc')
+    expect(hashTable.lookup('abc')).toBeUndefined()
+    expect(hashTable.lookup('acb')).toBe('married to Roger Rabbit')
   })
 
   test('should handle hash collisions', () => {
     const hashTable = new HashTable<string, string>(10)
-    hashTable.insert('Roger Rabbit', 'framed by Judge Doom')
-    hashTable.insert('Jessica Rabbit', 'married to Roger Rabbit')
-    expect(hashTable.lookup('Roger Rabbit')).toBe('framed by Judge Doom')
-    expect(hashTable.lookup('Jessica Rabbit')).toBe('married to Roger Rabbit')
+    hashTable.insert('abc', 'framed by Judge Doom')
+    hashTable.insert('acb', 'married to Roger Rabbit')
+    expect(hashTable.lookup('abc')).toBe('framed by Judge Doom')
+    expect(hashTable.lookup('acb')).toBe('married to Roger Rabbit')
+  })
+
+  test('should be capable of handling multiple key/value types', () => {
+    const hashTable = new HashTable<number | string, string>(10)
+    hashTable.insert(123, 'framed by Judge Doom')
+    hashTable.insert('abc', 'married to Roger Rabbit')
+    expect(hashTable.lookup(123)).toBe('framed by Judge Doom')
+    expect(hashTable.lookup('abc')).toBe('married to Roger Rabbit')
+
+    const hashTable2 = new HashTable<number | string, {name: string, toon: boolean}>(10)
+    hashTable2.insert('Roger Rabbit', { name: 'Roger Rabbit', toon: true })
+    hashTable2.insert('Jessica Rabbit', { name: 'Jessica Rabbit', toon: true })
+    expect(hashTable2.lookup('Roger Rabbit')).toEqual({ name: 'Roger Rabbit', toon: true })
+    expect(hashTable2.lookup('Jessica Rabbit')).toEqual({ name: 'Jessica Rabbit', toon: true })
   })
 })
--- src/hash-table/index.ts
+++ src/hash-table/index.ts
@@ -1,6 +1,11 @@
+type HashTableEntry<K, V> = {
+  key: K
+  value: V
+}
+
 class HashTable<K, V> {
   private size: number
-  private table: Array<V | undefined>
+  private table: Array<Array<HashTableEntry<K, V>>>
 
   get tableSize(): number {
     return this.size
@@ -8,7 +13,7 @@ class HashTable<K, V> {
 
   constructor(size: number) {
     this.size = size
-    this.table = new Array(size)
+    this.table = []
   }
 
   private hash(key: K): number {
@@ -27,22 +32,52 @@ class HashTable<K, V> {
       hash = (hash << 5) - hash + keyString.charCodeAt(i)
       hash |= 0 // Convert to 32bit integer
     }
+
     return Math.abs(hash) % this.size
   }
 
   insert(key: K, value: V) {
     const index = this.hash(key)
-    this.table[index] = value
+    
+    if (!this.table[index]) { this.table[index] = [] }
+
+    for (let entry of this.table[index]) {
+      if (entry.key === key) {
+        entry.value = value
+        return
+      }
+    }
+
+    this.table[index]!.push({ key, value })
   }
 
   lookup(key: K) {
     const index = this.hash(key)
-    return this.table[index]
+    const entries = this.table[index];
+
+    if (entries) {
+      for (let entry of entries) {
+        if (entry.key === key) {
+          return entry.value;
+        }
+      }
+    }
+
+    return undefined;
   }
 
   remove(key: K) {
     const index = this.hash(key)
-    this.table[index] = undefined
+    const entries = this.table[index];
+
+    if (entries) {
+      for (let i = 0; i < entries.length; i++) {
+        if (entries[i].key === key) {
+          entries.splice(i, 1);
+          return;
+        }
+      }
+    }
   }
 }
 
