eb23da4d53d5ed6a6426b7e882ef1700ce2477f5
Joe <joe_carpenito@yahoo.com>
2024-10-09 09:55:23 -0400

    small refactor and informational comments (#5)
    Co-authored-by: Joe Carpenito <johcarpe1@WKMZT2D96F6E.global.publicisgroupe.net>

--- src/binary-tree/index.ts
+++ src/binary-tree/index.ts
@@ -1,4 +1,4 @@
-export class Node<Number> {
+class Node<Number> {
   value: Number
   left: Node<Number> | null
   right: Node<Number> | null
@@ -10,6 +10,8 @@ export class Node<Number> {
   }
 
   findMin(): Node<Number> {
+    // we know that the minimum value in a binary search tree is the leftmost node
+    // so we recurse to the left until we find a node that has no left child
     if (!this.left) {
       return this
     } else {
@@ -40,21 +42,20 @@ export default class BinaryTree<Number> {
     }
 
     const recurseNodes = (node: Node<Number>): void => {
-      if (value < node.value) {
-        if (!node.left) {
-          node.left = new Node(value)
-          return
-        }
-
-        recurseNodes(node.left)
+      if (value < node.value && !node.left) {
+        // if the value we want to insert is less than the current node's value and
+        // the left child is null, we insert the value as the left child
+        node.left = new Node(value)
         return
-      }
-
-      if (!node.right) {
+      } else if (!node.right) {
+        // if the value we want to insert is greater than the current node's value and
+        // the right child is null, we insert the value as the right child
         node.right = new Node(value)
         return
       }
 
+      // if the value we want to insert is greater than or equal to the current node's
+      // value, we recurse to the right
       recurseNodes(node.right)
     }
 
@@ -65,13 +66,9 @@ export default class BinaryTree<Number> {
     const recurseNodes = (node: Node<Number> | null): Node<Number> | null => {
       if (!node) {
         return null
-      }
-
-      if (node.value === value) {
+      } else if (node.value === value) {
         return node
-      }
-
-      if (value < node.value) {
+      } else if (value < node.value) {
         return recurseNodes(node.left)
       }
 
@@ -85,20 +82,26 @@ export default class BinaryTree<Number> {
     const recurseNodes = (node: Node<Number> | null): Node<Number> | null => {
       if (!node) {
         return null
-      }
-
-      if (value < node.value) {
+      } else if (value < node.value) {
+        // if the value we want to remove is less than the current node's value,
+        // we recurse to the left
         node.left = recurseNodes(node.left)
       } else if (value > node.value) {
+        // if the value we want to remove is greater than the current node's value,
+        // we recurse to the right
         node.right = recurseNodes(node.right)
       } else {
         if (node.left === null && node.right === null) {
+          // if the node has no children, we can just remove it
           node = null
         } else if (node.left == null) {
+          // if the node has only a right child, we can replace the node with its right child
           node = node.right
         } else if (node.right == null) {
+          // if the node has only a left child, we can replace the node with its left child
           node = node.left
         } else {
+          // if the node has two children, we find the minimum value in the right subtree
           const minRight = node.right.findMin()
 
           node.value = minRight.value
