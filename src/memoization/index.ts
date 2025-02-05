0b7fe0f0cabfd5ecc8916479d2ce47934f964935
Joe Carpenito <johcarpe1@WKMZT2D96F6E.global.publicisgroupe.net>
2025-02-05 12:23:10 -0500

    refactor sequence implementation to meet spec expectations and refine tests

--- src/memoization/index.ts
+++ src/memoization/index.ts
@@ -1,12 +1,10 @@
 const memo: { [key: number]: number } = {}
 
-function fibonacci(n: number): number {
-  if (n <= 1) return n
+export const fibonacci = (n: number): number => {
+  if (n <= 0) return 0
+  if (n === 1) return 1
   if (memo[n]) return memo[n]
 
   memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
   return memo[n]
 }
-
-// Example usage:
-console.log(fibonacci(10)) // Output: 55
