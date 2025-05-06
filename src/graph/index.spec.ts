9ce7931c2bc6f59115fafbaab5330e846cc1041a
Joe <joe_carpenito@yahoo.com>
2025-05-06 15:13:51 -0400

    Add graph example (#19)
    * initial adjacency list graph
    
    * add meaningful JSDoc comments
    
    * initial readme
    
    * add adjacency graph to main and home readme
    
    * add breadth-first traversal for the graph
    
    * add depth traversal

--- src/graph/index.spec.ts
+++ src/graph/index.spec.ts
@@ -51,4 +51,38 @@ describe('AdjacencyListGraph', () => {
     const graph = new AdjacencyListGraph()
     expect(() => graph.getNeighbors('A')).toThrowError('Vertex does not exist')
   })
+
+  it('should perform a breadth-first search from a given node (vertex)', () => {
+    const graph = new AdjacencyListGraph()
+    graph.addVertex('A')
+    graph.addVertex('B')
+    graph.addVertex('C')
+    graph.addVertex('D')
+    graph.addEdge('A', 'B')
+    graph.addEdge('A', 'C')
+    graph.addEdge('C', 'D')
+    expect(graph.breadthSearch('A')).toEqual(['A', 'B', 'C', 'D'])
+    expect(graph.breadthSearch('C')).toEqual(['C', 'A', 'D', 'B'])
+
+    expect(() => graph.breadthSearch('E')).toThrowError(
+      'Start vertex does not exist'
+    )
+  })
+
+  it('should perform a depth-first search from a given node (vertex)', () => {
+    const graph = new AdjacencyListGraph()
+    graph.addVertex('A')
+    graph.addVertex('B')
+    graph.addVertex('C')
+    graph.addVertex('D')
+    graph.addEdge('C', 'D')
+    graph.addEdge('A', 'B')
+    graph.addEdge('A', 'C')
+    expect(graph.depthSearch('A')).toEqual(['A', 'B', 'C', 'D'])
+    expect(graph.depthSearch('C')).toEqual(['C', 'D', 'A', 'B'])
+
+    expect(() => graph.breadthSearch('E')).toThrowError(
+      'Start vertex does not exist'
+    )
+  })
 })
--- src/graph/index.ts
+++ src/graph/index.ts
@@ -107,4 +107,86 @@ export class AdjacencyListGraph {
     }
     return this.adjacencyList.get(vertex)!
   }
+
+  /**
+   * Performs a breadth-first search (BFS) starting from a given vertex.
+   * @param {string} startVertex The vertex to start the BFS from.
+   * @returns {string[]} An array of vertices visited in BFS order.
+   * @throws Error if the start vertex does not exist.
+   * @example
+   * ```typescript
+   * const graph = new AdjacencyListGraph()
+   * graph.addVertex('A')
+   * graph.addVertex('B')
+   * graph.addVertex('C')
+   * graph.addEdge('A', 'B')
+   * graph.addEdge('A', 'C')
+   * console.log(graph.breadthSearch('A')) // ['A', 'B', 'C']
+   * ```
+   */
+  breadthSearch(startVertex: string): string[] {
+    if (!this.adjacencyList.has(startVertex)) {
+      throw new Error('Start vertex does not exist')
+    }
+
+    const visited = new Set<string>()
+    const queue: string[] = [startVertex]
+    const result: string[] = []
+
+    while (queue.length > 0) {
+      const currentVertex = queue.shift()!
+      if (!visited.has(currentVertex)) {
+        visited.add(currentVertex)
+        result.push(currentVertex)
+
+        const neighbors = this.adjacencyList.get(currentVertex)!
+        for (const neighbor of neighbors) {
+          if (!visited.has(neighbor)) {
+            queue.push(neighbor)
+          }
+        }
+      }
+    }
+
+    return result
+  }
+
+  /**
+   * Performs a depth-first search (DFS) starting from a given vertex.
+   * @param {string} startVertex The vertex to start the DFS from.
+   * @returns {string[]} An array of vertices visited in DFS order.
+   * @throws Error if the start vertex does not exist.
+   * @example
+   * ```typescript
+   * const graph = new AdjacencyListGraph()
+   * graph.addVertex('A')
+   * graph.addVertex('B')
+   * graph.addVertex('C')
+   * graph.addEdge('A', 'B')
+   * graph.addEdge('A', 'C')
+   * console.log(graph.depthSearch('A')) // ['A', 'B', 'C']
+   * ```
+   */
+  depthSearch(startVertex: string): string[] {
+    if (!this.adjacencyList.has(startVertex)) {
+      throw new Error('Start vertex does not exist')
+    }
+
+    const visited = new Set<string>()
+    const result: string[] = []
+
+    const dfs = (vertex: string) => {
+      if (visited.has(vertex)) return
+      visited.add(vertex)
+      result.push(vertex)
+
+      const neighbors = this.adjacencyList.get(vertex)!
+      for (const neighbor of neighbors) {
+        dfs(neighbor)
+      }
+    }
+
+    dfs(startVertex)
+    return result
+  }
 }
